44cc6ac2b3732bcf4cd43fe02aaa708090998dad
zorojurio <zorojurio@gmail.com>
2021-11-22 14:10:59 +0530

    list view

--- custom/my_library/__manifest__.py
+++ custom/my_library/__manifest__.py
@@ -28,6 +28,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/book.xml',
+        'views/web_book.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- custom/my_library/controllers/controllers.py
+++ custom/my_library/controllers/controllers.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
 
-class LibraryBookView(http.Controller):
+class Book(http.Controller):
     @http.route('/library/books/', auth='public', website=True)
     def patient_webform(self, **kw):
-        return http.request.render('my_library.create_patient', {})
+        books = request.env['library.book'].sudo().search([])
+        print('printing books----------------------------', books)
+        for book in books:
+            print('-----------printing books -----------------', book.name)
+        return request.render('my_library.books_page', {
+            'books': books
+        })
 
+
+# class Hospital(http.Controller):
+#     @http.route('/patient/webform/',  website=True, auth='public')
+#     def patient_webform(self, **kw):
+#         patients = request.env['hosptial.patient'].sudo().search([])
+#         print('patients----------', patients)
+#         return request.render('om_hospital.patients_page', {
+#             'patients': patients
+#         })
 
 #     @http.route('/custom/my_library/custom/my_library/objects/<model("custom/my_library.custom/my_library"):obj>/', auth='public')
 #     def object(self, obj, **kw):
